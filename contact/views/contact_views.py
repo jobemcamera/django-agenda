fc9f973036cc1f4953c5b35fabad5d9294da03fa
Jobe Moraes <jobecamera@gmail.com>
2024-02-18 19:53:00 -0300

    feat: create contact view to show a specific contact

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 
@@ -12,5 +12,20 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    # single_contact = get_object_or_404(Contact.filter(pk=contact_id).first())
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
