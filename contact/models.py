49e50f4ff8108a9966defd69e3fee37a4e8c845d
Jobe Moraes <jobecamera@gmail.com>
2024-02-18 12:29:10 -0300

    build: create MEDIA_URL and MEDIA_ROOT to salve ImageField

--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,8 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
-    
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='pictures/%Y/%m/', blank=True)
+
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
