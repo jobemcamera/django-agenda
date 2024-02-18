da7d44fa6b91ec5076172826775f792845cd2164
Jobe Moraes <jobecamera@gmail.com>
2024-02-18 19:12:56 -0300

    feat: render index table all contacts

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 
 def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    context = {
+        'contacts': contacts,
+    }
+
     return render(
         request,
-        'contact/index.html'
+        'contact/index.html',
+        context
     )
