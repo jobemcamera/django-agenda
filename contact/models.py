4494e3eeff55d6b58b736cf24086babe51b5a410
Jobe Moraes <jobecamera@gmail.com>
2024-02-18 17:55:53 -0300

    feat: add owner in Contact  as foreign key

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -24,6 +25,8 @@ class Contact(models.Model):
     picture = models.ImageField(upload_to='pictures/%Y/%m/', blank=True)
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(
+        User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
