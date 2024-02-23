4c02a4885e6571e77e5edf86e9c204bb8766ee83
Jobe Moraes <jobecamera@gmail.com>
2024-02-23 09:05:30 -0300

    refactor: redirect after search and filter data

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    context = {
+        'contacts': contacts,
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    # get o name do input 'q' e strip remove os espaços vazios
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    # Q permite que a consulta seja com OR
+    contacts = Contact.objects.filter(show=True).filter(
+        Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value) | Q(phone__icontains=search_value) | Q(email__icontains=search_value)).order_by('-id')
 
     context = {
         'contacts': contacts,
