d599d0ac2a2ea229cace229ec814bfbbad2acd5c
Jobe Moraes <jobecamera@gmail.com>
2024-02-25 10:41:09 -0300

    refactor: create forms.py file

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,25 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Q
-from django.core.paginator import Paginator
-from contact.models import Contact
-from django import forms
-from django.core.exceptions import ValidationError
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ('first_name', 'last_name', 'phone', 'email',)
-
-    def clean(self):
-        clenead_data = self.cleaned_data
-
-        self.add_error('first_name', ValidationError(
-            'Mensagem de erro', code='invalid'))
-        self.add_error('last_name', ValidationError(
-            'Mensagem de erro', code='invalid'))
-        
-        return super().clean()
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 
 def create(request):
