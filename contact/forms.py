a8cf47d8cd4b7676f57eece293be88ac83bd29cf
Jobe Moraes <jobecamera@gmail.com>
2024-02-25 14:53:47 -0300

    feat: possibility to send files (img) to db

--- contact/forms.py
+++ contact/forms.py
@@ -4,7 +4,15 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',                
+            }
+        )
+    )
+    
     class Meta:
         model = models.Contact
-        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category')
+        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category', 'picture')
 
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -8,7 +8,7 @@ def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form = ContactForm(data=request.POST)
+        form = ContactForm(data=request.POST, files=request.FILES)
 
         context = {
             'form': form,
@@ -42,7 +42,8 @@ def update(request, contact_id):
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form = ContactForm(data=request.POST, instance=contact)
+        form = ContactForm(data=request.POST,
+                           files=request.FILES, instance=contact)
 
         context = {
             'form': form,
