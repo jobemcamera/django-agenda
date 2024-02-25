948263b21a892b102121c76d5765e82727bf89c3
Jobe Moraes <jobecamera@gmail.com>
2024-02-25 14:13:51 -0300

    feat: view delete to delete a contact

--- contact/urls.py
+++ contact/urls.py
@@ -6,9 +6,10 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
-    
+
     # CRUD
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 ]
