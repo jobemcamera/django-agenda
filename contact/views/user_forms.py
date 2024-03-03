30fbc0331c76246fed2e0d8b88071658e20812ba
Jobe Moraes <jobecamera@gmail.com>
2024-03-03 11:48:31 -0300

    feat: login form

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
 from contact.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+
 
 def register(request):
     form = RegisterForm()
-    
+
     if request.method == 'POST':
         form = RegisterForm(request.POST)
-        
+
         if form.is_valid():
             form.save()
             messages.success(request, 'User has register')
-            return redirect('contact:index')
+            return redirect('contact:login')
 
     context = {
         'form': form
@@ -22,3 +25,33 @@ def register(request):
         'contact/register.html',
         context,
     )
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'User successfully logged in')
+            return redirect('contact:index')
+        
+        messages.error(request, 'User login unsuccessful')
+
+    context = {
+        'form': form
+    }
+
+    return render(
+        request,
+        'contact/login.html',
+        context,
+    )
+
+def logout_view(request):
+    auth.logout(request)
+    
+    return redirect('contact:login')
